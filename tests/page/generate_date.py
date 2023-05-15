4375cc9ec84a192280be8384105c7b9e5f902cc4
Andrey Pyanykh <>
2023-05-15 23:25:59 +0500

    fix generate_fibonacci_number

--- tests/page/generate_date.py
+++ tests/page/generate_date.py
@@ -5,6 +5,6 @@ def generate_fibonacci_number(n: int) -> int:
     :return: Число Фибоначчи с заданным номером в последовательности
     """
     tmp = [0, 1]
-    while len(tmp) < n:
+    while len(tmp) <= n:
         tmp.append(tmp[-1]+tmp[-2])
     return tmp[n]
