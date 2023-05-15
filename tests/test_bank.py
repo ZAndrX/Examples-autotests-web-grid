c108b625967c60f1d35eb53b19880dc21ef379a6
Andrey Pyanykh <>
2023-05-15 20:38:49 +0500

    add title for tests and delete test_open_page

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,14 +1,10 @@
 import time
+import allure
+
 from .page.base_page import BasePage
 from .page.login_page import LoginPage
 
-
-def test_open_page(browser):
-    page = BasePage(browser)
-    page.open_page('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
-    time.sleep(5)
-
-
+@allure.title('Проверка работы финансовых операций "Harry Potter"')
 def test_transactions_customer_user(browser):
     page = LoginPage(browser)
     page.open_page('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
