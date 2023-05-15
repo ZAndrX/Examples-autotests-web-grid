6957d28d187094d397124e201885140889b1aee2
Andrey Pyanykh <>
2023-05-15 23:26:25 +0500

    add locators for account

--- tests/page/locators.py
+++ tests/page/locators.py
@@ -9,3 +9,17 @@ class BankingProjectLoginPage:
 class BankingProjectCustomerPage:
     form_select_user = (By.CSS_SELECTOR, '#userSelect')
     button_login = (By.CSS_SELECTOR, 'button[type="submit"]')
+
+
+class BankingProjectAccount:
+    balance = (By.CSS_SELECTOR, '.center>strong:nth-child(2)')
+    button_transactions = (By.CSS_SELECTOR, 'button[ng-click="transactions()"]')
+    button_deposit = (By.CSS_SELECTOR, 'button[ng-click="deposit()"]')
+    button_withdrawl = (By.CSS_SELECTOR, 'button[ng-click="withdrawl()"]')
+    input = (By.CSS_SELECTOR, 'input[type="number"]')
+    button_submit = (By.CSS_SELECTOR, 'button[type="submit"]')
+
+
+class BankingProjectTransactions:
+    table = (By.CSS_SELECTOR, ".table")
+    table_headers = (By.CSS_SELECTOR, ".table>thead>tr:nth-child(1)>td")
