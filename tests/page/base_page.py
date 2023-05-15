3471189b5faead6a01783fdf2ca41c90da653c13
Andrey Pyanykh <>
2023-05-15 20:14:49 +0500

    add wait element

--- tests/page/base_page.py
+++ tests/page/base_page.py
@@ -1,6 +1,8 @@
 import allure
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+from selenium.webdriver.remote.webdriver import WebElement
 
 
 class BasePage:
@@ -14,3 +16,24 @@ class BasePage:
         :param url: Адрес страницы
         """
         self.browser.get(url)
+
+    @allure.step('Ожидание кликабельности элемента')
+    def wait_clickable_element(self, locator: tuple[str, str], timeout: int = 5) -> WebElement:
+        """
+        Ожидание кликабельности элемента на странице по локатору
+        :param locator: Локатор элемента
+        :param timeout: Максимальное время ожидания кликабельности элемента
+        :return: Кликабельный элемент найденный по локатору на странице
+        """
+        return Wait(self.browser, timeout).until(
+            expected_conditions.element_to_be_clickable(locator)
+        )
+
+    def wait_element_present(self, locator: tuple[str, str], timeout: int = 5) -> WebElement:
+        """
+        Ожидание элемента на странице по локатору
+        :param locator: Локатор элемента
+        :param timeout: Максимальное время ожидания появления элемента
+        :return: Элемент найденный по локатору на странице
+        """
+        return Wait(self.browser, timeout).until(expected_conditions.presence_of_element_located(locator))
