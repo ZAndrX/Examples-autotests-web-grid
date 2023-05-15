ea0bea2ac24503efbc1f8cba55625e1ec8244009
Andrey Pyanykh <>
2023-05-16 02:00:26 +0500

    update README.md, conftest.py, requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Examples-autotests-web-grid
+Для запуска окружения используйте:
+`docker-compose up --build -d`
+
+### Для запуска тестов используйте:
+`pytest -sv --alluredir=allure_folder --fib=10 `
+
+Где `--fib` номер числа Фибоначчи
+
+### Также можно использовать: 
+--browser_name - имя браузера \
+--browser_version - версия браузера \
+--url - адрес Grid
+
+### Для сборки отчета allure используйте:
+`allure serve allure_folder`
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox or edge")
     parser.addoption('--browser_version', action='store', default='113.0',
                      help="Choose browser version")
+    parser.addoption('--url', action='store', default='http://localhost:4444',
+                     help="Choose url grid")
     parser.addoption('--fib', action='store', default=10,
                      help="Choose fibonacci number")
 
@@ -15,13 +17,14 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser_version = request.config.getoption("browser_version")
-    fib = request.config.getoption("fib")
+    fib = int(request.config.getoption("fib"))
+    url = request.config.getoption("url")
     capabilities = {
         "browserName": browser_name,
         "browserVersion": browser_version
     }
     browser = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+        command_executor=f'{url}/wd/hub',
         desired_capabilities=capabilities
     )
 
