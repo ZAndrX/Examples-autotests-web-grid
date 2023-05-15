e8be27b9b56fbc844c79a65375421f566a5aa4ec
Andrey Pyanykh <>
2023-05-16 00:58:26 +0500

    add attach_transactions

--- tests/page/user_page.py
+++ tests/page/user_page.py
@@ -1,20 +1,22 @@
 import time
-
 import allure
 from selenium.common import StaleElementReferenceException
-
 from .base_page import BasePage
-from .locators import BankingProjectAccount
+from .locators import BankingProjectAccount, BankingProjectTransactions
 
 
 class UserPage(BasePage):
-    @allure.step('нажатие на кнопку "Deposit"')
+    @allure.step('Нажатие на кнопку "Deposit"')
     def select_deposit(self) -> None:
         self.wait_clickable_element(BankingProjectAccount.button_deposit).click()
 
-    @allure.step('нажатие на кнопку "Withdrawl"')
+    @allure.step('Нажатие на кнопку "Withdrawl"')
     def select_withdrawl(self) -> None:
         self.wait_clickable_element(BankingProjectAccount.button_withdrawl).click()
+
+    @allure.step('Нажатие на кнопку "Transactions"')
+    def select_transactions(self) -> None:
+        self.wait_clickable_element(BankingProjectAccount.button_transactions).click()
 
     @allure.step('Ввод количества единиц для операции')
     def input_amount(self, n: int) -> None:
@@ -68,3 +70,21 @@ class UserPage(BasePage):
         :return: Количество единиц баланса
         """
         return self.get_text(BankingProjectAccount.balance)
+
+    @allure.step('Преобразование транзакций в словарь')
+    def get_table_transactions(self) -> list[dict]:
+        """
+        Преобразование транзакций в словарь
+        :return: Словарь с данными таблицы table_to_dict
+        """
+        self.select_transactions()
+        self.wait_element_present(BankingProjectTransactions.table_headers)
+        return self.table_to_dict(locator_table=BankingProjectTransactions.table, locator_headers=BankingProjectTransactions.table_headers)
+
+    @allure.step('Прикрепление csv файла')
+    def attach_transactions(self, name_file: str, table_dict: list[dict]) -> None:
+        """
+        :param table_dict: Словарь с данными таблицы table_to_dict
+        :param name_file: Имя файла
+        """
+        self.attach_table(table_dict, name_file)
